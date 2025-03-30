96a21d17f2d04be2b414cd787ea92d5a5c8c2cd1
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-03-30 12:42:29 +0100

    feat: move authentication model into accounts app

--- event_management/events/models.py
+++ event_management/events/models.py
@@ -1,22 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from accounts.models import CustomUser
 
 
 # Create your models here.
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.email} - {self.username}"
-
-
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     location = models.CharField(max_length=250)
     capacity = models.IntegerField()
-    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    organizer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_date = models.DateField(auto_now_add=True)
     date = models.DateField()
     time = models.TimeField()
